8bb2c6b6f892749b23d67a4e1753759783f61eea
Axel Jeremy <axeljeremy2802@gmail.com>
2026-01-16 09:23:05 +0700

    simpen kode 1 dimensi bit string

---
+++ Mosaic1D/Coordinate.java
@@ -0,0 +1,108 @@
+package Mosaic;
+
+/**
+ * Class Coordinate mempresentasikan papan mosaic puzzle yang berisikan clue,
+ * class ini akan menyimpan koordinat atau lokasi
+ * dan angka pada titik itu yang menunjukan berapa banyak kotak hitam yang harus
+ * ada di sekitarnya termasukdi titik tersebut
+ * 
+ * Sumber: ...
+ * 
+ * @author Axel
+ * 
+ */
+public class Coordinate {
+    private int x; // baris
+    private int y; // kolom
+    private int value; // berapa kotak item yang harus ada di neighbor
+
+    /**
+     * konstruktor untuk membuat objek Coordinate
+     *
+     * @param x Indeks Baris
+     * @param y Indeks Kolom
+     * @param value Nilai angka petunjuk pada posisi tersebut
+     */
+    public Coordinate(int x, int y, int value) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+
+    /**
+     * ambil posisi baris
+     * @return Indeks baris
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * atur posisi baris
+     * @return Indeks baris baru
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * ambil posisi kolom
+     * @return Indeks kolom
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * atur posisi kolom
+     * @return Indeks kolom baru
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * ambil nilai clue
+     * @return Indeks kolom
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * atur nilai clue
+     * @return Indeks kolom baru
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * hitung jumlah maksimum tetangga yang mungkin dimiliki oleh koordinat tertentu
+     * Logika:
+     * Pojok (Corner): Memiliki 4 tetangga (termasuk diri sendiri)
+     * Tepi (Edge): Memiliki 6 tetangga
+     * Tengah (Middle): Memiliki 9 tetangga (blok 3x3 penuh)
+     *
+     * @param n Ukuran grid
+     * @return Jumlah maksimal sel dalam jangkauan clue (4, 6, 9)
+     */
+    public int findSumNeighbor(int n) {
+        // Konversi ukuran grid (n) menjadi n-1
+        n--;
+
+        if (this.x == 0 || this.x == n)
+            if (this.y == 0 || this.y == n)
+                // Kasus Pojok Kiri-Atas, Kanan-Atas, Kiri-Bawah, Kanan-Bawah
+                return 4;
+            else
+                // Kasus pinggir Atas atau Bawah (bukan pojok)
+                return 6;
+        else if (this.y == 0 || this.y == n)
+            // Kasus Tepi kiri atau kanan (tapi bukan pojok)
+            return 6;
+        else
+            // tengah
+            return 9;
+    }
+}
---
+++ Mosaic1D/Crossover.java
@@ -0,0 +1,152 @@
+package Mosaic;
+
+import java.util.Random;
+
+/**
+ * Class crossOver bertugas untuk melakukan kombinasi dari 2 parent yang sbeleumnya terpilih
+ * yang akan menghasilkan anak nantinya
+ * 
+ * Metode:
+ * 1. Uniform Crossover
+ * 2. Single Point crossover
+ * 3. Two point crossover
+ * 
+ * @author Axel, Davin, Keane
+ * 
+ */
+public class Crossover {
+    Random MyRand;
+
+    /**
+     * Konstraktor untuk crossover
+     * 
+     * @param MyRand Generator untuk angka acak
+     */
+    public Crossover(Random MyRand) {
+        this.MyRand = MyRand;
+    }
+    /**
+     * Melakukan Uniform Crossover
+     * Setiap gen anak akan dipilih secara acak dari salah satu orang tua
+     *
+     * @param other Parent kedua (pasangan)
+     * @return Array berisi 2 Chromosome anak
+     */
+    public Chromosome[] uniformCrossover(Chromosome current, Chromosome other) {
+        Chromosome child1 = new Chromosome(MyRand);
+        Chromosome child2 = new Chromosome(MyRand);
+
+        int geneLength = current.getGene().length;
+        
+        int[] currentGene = current.getGene();
+        int[] otherGene = other.getGene();
+
+        int[] child1Gene = new int[geneLength];
+        int[] child2Gene = new int[geneLength];
+
+        for (int i = 0; i < geneLength; i++) {
+            // Acak pengambilan gen untuk Anak 1
+            child1Gene[i] = MyRand.nextBoolean() ? currentGene[i] : otherGene[i];
+            // Acak pengambilan gen untuk Anak 2
+            child2Gene[i] = MyRand.nextBoolean() ? otherGene[i] : currentGene[i];
+        }
+
+        child1.setGene(child1Gene);
+        child2.setGene(child2Gene);
+
+        return new Chromosome[] { child1, child2 };
+    }
+
+    /**
+     * Melakukan Single Point Crossover
+     * Memotong kromosom di satu titik acak, lalu menukar bagian ekornya
+     *
+     * @param other Parent kedua
+     * @return Array berisi 2 Chromosome anak
+     */
+    public Chromosome[] singlePointCrossover(Chromosome current, Chromosome other) {
+        // Tentukan titik potong
+        // int potongan = this.gene.length / 2;
+        int geneLength = current.getGene().length;
+        
+        int potongan = MyRand.nextInt(geneLength);
+
+        Chromosome child1 = new Chromosome(MyRand);
+        Chromosome child2 = new Chromosome(MyRand);
+        
+        int[] currentGene = current.getGene();
+        int[] otherGene = other.getGene();
+
+        int[] child1Gene = new int[geneLength];
+        int[] child2Gene = new int[geneLength];
+
+        // Lakukan crossover dengan menyalin gen
+        for (int i = 0; i < geneLength; i++) {
+            if (i <= potongan) {
+                // Anak 1 mengambil dari Parent 1 (this)
+                child1Gene[i] = currentGene[i];
+                // Anak 2 mengambil dari Parent 2 (other)
+                child2Gene[i] = otherGene[i];
+            } else {
+                // Anak 1 mengambil dari Parent 2 (other)
+                child1Gene[i] = otherGene[i];
+                // Anak 2 mengambil dari Parent 1 (this)
+                child2Gene[i] = currentGene[i];
+            }
+        }
+
+        child1.setGene(child1Gene);
+        child2.setGene(child2Gene);
+
+        return new Chromosome[] { child1, child2 };
+    }
+
+    /**
+     * Melakukan Two Point Crossover dengan memilih dua titik potong,
+     * lalu menukar segmen di antara kedua titik tersebut
+     *
+     * @param other Parent kedua
+     * @return Array berisi 2 Chromosome anak
+     */
+    public Chromosome[] twoPointCrossover(Chromosome current, Chromosome other) {
+        Chromosome child1 = new Chromosome(MyRand);
+        Chromosome child2 = new Chromosome(MyRand);
+
+        int geneLength = current.getGene().length;
+        
+        int[] currentGene = current.getGene();
+        int[] otherGene = other.getGene();
+
+        int[] child1Gene = new int[geneLength];
+        int[] child2Gene = new int[geneLength];
+
+        // tentukan titik potong 1 dan 2
+        int point1 = MyRand.nextInt(geneLength);
+        int point2 = MyRand.nextInt(geneLength);
+
+        // Buat agar titik potong start lebih kecil dari end
+        int start = Math.min(point1, point2);
+        int end = Math.max(point1, point2);
+
+        for (int i = 0; i < geneLength; i++) {
+            // kalau ada di luar titik potong, pertahankan seperti awal
+            if (i < start || i > end) {
+                // Anak 1 mengambil dari Parent 1 (this)
+                child1Gene[i] = currentGene[i];
+                // Anak 2 mengambil dari Parent 2 (other)
+                child2Gene[i] = otherGene[i];
+            } else {
+                // Kalau di dalam titik potong, tuker
+                // Anak 1 mengambil dari Parent 2 (other)
+                child1Gene[i] = otherGene[i];
+                // Anak 2 mengambil dari Parent 1 (this)
+                child2Gene[i] = currentGene[i];
+            }
+        }
+
+        child1.setGene(child1Gene);
+        child2.setGene(child2Gene);
+
+        return new Chromosome[] { child1, child2 };
+    }
+}
